0a1011cd6a959fa73dbfdef08d01280af1191daa
Hibikine Kage <goodsmile314159@gmail.com>
2018-05-28 12:53:32 +0900

    feat: CPUバトルのボタンを追加

---
+++ src/rooms/cpu.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface Props {
+  startCpuBattle : () => any;
+}
+const Cpu = (props : Props) => <div>
+  <button onClick={props.startCpuBattle}>CPU Battle</button>
+</div>
+export default Cpu;
--- src/rooms/index.tsx
+++ src/rooms/index.tsx
@@ -4,19 +4,20 @@ import {connect, Dispatch} from 'react-redux';
 import {Room} from './room';
 import * as React from 'react';
 import {State} from '../ducks';
-import {REQUEST_ROOM_LIST, ADD_ROOM} from './ducks';
+import {REQUEST_ROOM_LIST, ADD_ROOM, START_CPU_BATTLE} from './ducks';
 import socket from '../app/socket';
 import AddRoom from './add-room';
 import {dummyFunction} from '../utils';
+import Cpu from './cpu';
 interface Props {
   fetchRoomList : () => any;
   rooms : ClientRoom[];
   addRoom : (roomName : string) => any;
+  startCpuBattle : () => any;
 }
 class Rooms extends React.Component < Props > {
   constructor(props : Props) {
     super(props)
-    console.log('fetchRoomList');
     socket.on('connect', () => {
       this
         .props
@@ -26,20 +27,26 @@ class Rooms extends React.Component < Props > {
   render() {
     return (
       <div>
-        {this
-          .props
-          .rooms
-          .map(room => <Room key={room.roomId} joinRoom={dummyFunction} {...room}/>)
-}<AddRoom addRoom={this.props.addRoom}/>
+        <h1>Lobby</h1>
+        <section>
+          <h2>Room List</h2>
+          {this
+            .props
+            .rooms
+            .map(room => <Room key={room.roomId} joinRoom={dummyFunction} {...room}/>)
+}</section>
+        <AddRoom addRoom={this.props.addRoom}/>
+        <Cpu startCpuBattle={this.props.startCpuBattle}/>
       </div>
-    )
+    );
   }
 }
 export default connect((state : State) => ({rooms: state.rooms.rooms}), (dispatch : Dispatch) => ({
   fetchRoomList: () => dispatch({type: REQUEST_ROOM_LIST}),
   addRoom: (roomName : string) => dispatch({type: ADD_ROOM, payload: {
       roomName
-    }})
+    }}),
+  startCpuBattle: () => dispatch({type: START_CPU_BATTLE})
 }), (stateProps, dispatchProps) => {
   return {
     ...stateProps,
